6a72b75b0b4a2694fcf9ff905d5c172fe1d67c0f
Jake Wharton <jakew@google.com>
2017-12-16 23:41:21 -0500

    Generated factory should be package-private.

--- compiler/src/main/kotlin/se/ansman/kotshi/FactoryProcessingStep.kt
+++ compiler/src/main/kotlin/se/ansman/kotshi/FactoryProcessingStep.kt
@@ -60,7 +60,7 @@ class FactoryProcessingStep(
         val (genericAdapters, regularAdapters) = adapters.entries.partition { it.value is ParameterizedTypeName }
 
         val typeSpec = TypeSpec.classBuilder(generatedName.simpleName())
-                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                .addModifiers(Modifier.FINAL)
                 .superclass(TypeName.get(factoryType.asType()))
                 .addMethod(MethodSpec.methodBuilder("create")
                         .addModifiers(Modifier.PUBLIC)
