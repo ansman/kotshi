bfe44e5bef5f5b72a31fb4deacc6afa97fab8784
Nicklas Ansman Giertz <nicklas@ansman.se>
2017-12-22 17:44:03 -0500

    Add an additional test case

--- tests/src/main/kotlin/se/ansman/kotshi/ClassWithBoundGeneric.kt
+++ tests/src/main/kotlin/se/ansman/kotshi/ClassWithBoundGeneric.kt
@@ -2,3 +2,19 @@ package se.ansman.kotshi
 
 @JsonSerializable
 data class ClassWithBoundGeneric<out Data>(val data: List<Data>)
+
+@JsonSerializable
+data class ParameterizedModel<out Data>(val data: Data)
+
+@JsonSerializable
+data class TripleParameterizedModel<out Data, out T : CharSequence, out E : Any>(
+        val e: E,
+        val data: Data,
+        val t: T
+)
+
+@JsonSerializable
+data class TypeCeption<out D : Any>(
+        val tpm: TripleParameterizedModel<ParameterizedModel<IntArray>, String, D>,
+        val pm: ParameterizedModel<List<D>>
+)
