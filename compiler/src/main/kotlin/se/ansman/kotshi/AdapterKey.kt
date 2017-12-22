afcaa937da15e8aa8403322cbae74af762d44fc1
Nicklas Ansman Giertz <nicklas@ansman.se>
2017-12-22 17:25:39 -0500

    Fix an issue with generics being used as generic type arguments (#42)
    This fixes #41

--- compiler/src/main/kotlin/se/ansman/kotshi/AdapterKey.kt
+++ compiler/src/main/kotlin/se/ansman/kotshi/AdapterKey.kt
@@ -1,9 +1,6 @@
 package se.ansman.kotshi
 
-import com.squareup.javapoet.CodeBlock
-import com.squareup.javapoet.ParameterizedTypeName
-import com.squareup.javapoet.TypeName
-import com.squareup.javapoet.TypeVariableName
+import com.squareup.javapoet.*
 import com.squareup.moshi.Types
 import javax.lang.model.element.Element
 
@@ -13,22 +10,29 @@ data class AdapterKey(
 ) {
 
     val isGeneric: Boolean
-        get() = type is TypeVariableName
+        get() = when (type) {
+            is TypeVariableName -> true
+            is WildcardTypeName -> false
+            else -> false
+        }
 
-    fun asRuntimeType(): CodeBlock = type.asRuntimeType()
+    fun asRuntimeType(typeVariableAccessor: (TypeVariableName) -> CodeBlock): CodeBlock =
+            type.asRuntimeType(typeVariableAccessor)
 
-    private fun TypeName.asRuntimeType(): CodeBlock = when (this) {
-        is ParameterizedTypeName ->
-            CodeBlock.builder()
-                    .add("\$T.newParameterizedType(\$T.class", Types::class.java, rawType)
-                    .apply {
-                        for (typeArgument in typeArguments) {
-                            add(", ")
-                            add(typeArgument.asRuntimeType())
-                        }
-                    }
-                    .add(")")
-                    .build()
-        else -> CodeBlock.of("\$T.class", this)
-    }
+    private fun TypeName.asRuntimeType(typeVariableAccessor: (TypeVariableName) -> CodeBlock): CodeBlock =
+            when (this) {
+                is ParameterizedTypeName ->
+                    CodeBlock.builder()
+                            .add("\$T.newParameterizedType(\$T.class", Types::class.java, rawType)
+                            .apply {
+                                for (typeArgument in typeArguments) {
+                                    add(", ")
+                                    add(typeArgument.asRuntimeType(typeVariableAccessor))
+                                }
+                            }
+                            .add(")")
+                            .build()
+                is TypeVariableName -> typeVariableAccessor(this)
+                else -> CodeBlock.of("\$T.class", this)
+            }
 }
