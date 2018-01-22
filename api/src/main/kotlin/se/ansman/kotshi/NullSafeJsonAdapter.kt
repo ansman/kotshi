0ac395fdf21b45e43e7530d34931828680f8616f
Nicklas Ansman Giertz <nicklas@ansman.se>
2018-01-22 18:59:00 -0500

    Make the NullSafeJsonAdapter constructor be protected

--- api/src/main/kotlin/se/ansman/kotshi/NullSafeJsonAdapter.kt
+++ api/src/main/kotlin/se/ansman/kotshi/NullSafeJsonAdapter.kt
@@ -2,6 +2,6 @@ package se.ansman.kotshi
 
 import com.squareup.moshi.JsonAdapter
 
-abstract class NullSafeJsonAdapter<T: Any>(private val toString: String) : JsonAdapter<T>() {
+abstract class NullSafeJsonAdapter<T: Any> protected constructor(private val toString: String) : JsonAdapter<T>() {
     override fun toString(): String = toString
 }
