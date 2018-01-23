6b64b31914c6452cb10486051d5dd8aea60a1eee
Nicklas Ansman Giertz <nicklas@ansman.se>
2018-01-22 19:00:35 -0500

    Make NullSafeJsonAdapter.toString final

--- api/src/main/kotlin/se/ansman/kotshi/NullSafeJsonAdapter.kt
+++ api/src/main/kotlin/se/ansman/kotshi/NullSafeJsonAdapter.kt
@@ -3,5 +3,5 @@ package se.ansman.kotshi
 import com.squareup.moshi.JsonAdapter
 
 abstract class NullSafeJsonAdapter<T: Any> protected constructor(private val toString: String) : JsonAdapter<T>() {
-    override fun toString(): String = toString
+    final override fun toString(): String = toString
 }
