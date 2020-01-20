cbe238e944efad62c97bb85dd5264e4a3a2c22fb
Nicklas Ansman Giertz <nicklas@ansman.se>
2020-01-20 11:13:15 -0500

    Fix annotating generic types with an annotation (#136)
    This is required when using @RawValue on Android for example.

--- tests/src/main/kotlin/se/ansman/kotshi/GenericClass.kt
+++ tests/src/main/kotlin/se/ansman/kotshi/GenericClass.kt
@@ -1,4 +1,11 @@
 package se.ansman.kotshi
 
+@Target(AnnotationTarget.TYPE)
+annotation class SomeTypeAnnotation
+
 @JsonSerializable
-data class GenericClass<out T : CharSequence, out C : Collection<T>>(val collection: C, val value: T)
+data class GenericClass<out T : CharSequence, out C : Collection<T>>(
+    val collection: C,
+    val value: T,
+    val valueWithTypeAnnotation: @SomeTypeAnnotation T
+)
