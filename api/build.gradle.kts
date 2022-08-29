08c229f4383fbcead7b5204919ee4a637a509f03
Nicklas Ansman Giertz <nicklas@ansman.se>
2022-08-29 14:43:55 -0400

    Fix the source link in dokka

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -33,7 +33,7 @@ tasks.withType<org.jetbrains.dokka.gradle.DokkaTask> {
         configureEach {
             sourceLink {
                 localDirectory.set(file("src/main/kotlin"))
-                remoteUrl.set(URL("https://github.com/ansman/kotshi/blob/master/src/main/kotlin"))
+                remoteUrl.set(URL("https://github.com/ansman/kotshi/blob/main/api/src/main/kotlin"))
                 remoteLineSuffix.set("#L")
             }
             externalDocumentationLink { url.set(URL("https://square.github.io/okio/2.x/okio/")) }
