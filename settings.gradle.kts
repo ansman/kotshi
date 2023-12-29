3c7037b6cdb5579f93b06541b4e9a34ff1982ea0
Nicklas Ansman <nicklas@ansman.se>
2023-12-29 08:22:08 -0500

    Remove gradle enterprise

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,10 +5,6 @@ pluginManagement {
     }
 }
 
-plugins {
-    id("com.gradle.enterprise") version "3.15.1"
-}
-
 rootProject.name = "kotshi"
 include("compiler")
 include("api")
@@ -26,12 +22,9 @@ dependencyResolutionManagement {
     }
 }
 
-gradleEnterprise {
-    if (System.getenv("CI") != null) {
-        buildScan {
-            publishAlways()
-            termsOfServiceUrl = "https://gradle.com/terms-of-service"
-            termsOfServiceAgree = "yes"
-        }
+if (System.getenv("CI") != null) {
+    extensions.findByName("buildScan")?.withGroovyBuilder {
+        setProperty("termsOfServiceUrl", "https://gradle.com/terms-of-service")
+        setProperty("termsOfServiceAgree", "yes")
     }
 }
