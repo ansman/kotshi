2d68de1d7088e8f9a55bffe59531189ac60915de
Nicklas Ansman <nicklas@ansman.se>
2025-04-27 12:51:58 -0400

    Disable CC while publishing

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ./gradlew clean
-./gradlew publishAllPublicationsToMavenCentralRepository \
+./gradlew publishAllPublicationsToMavenCentralRepository --no-configuration-cache \
   -PsignArtifacts=true \
   -Psigning.gnupg.executable=/opt/homebrew/bin/gpg \
   -Psigning.gnupg.keyName=$(op --account my.1password.com read op://private/GnuPG/keyID | xargs) \
