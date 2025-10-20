7ba13c46b06bd459631ef1a1a0753596e31b4a4c
Goooler <wangzongler@gmail.com>
2025-10-20 15:55:09 +0800

    Remove unnecessary dependencies for the symbol processor
    ```diff
    69,89d68
    <           "group": "com.google.auto.service",
    <           "module": "auto-service-annotations",
    <           "version": {
    <             "requires": "1.1.1"
    <           }
    <         },
    <         {
    <           "group": "net.ltgt.gradle.incap",
    <           "module": "incap",
    <           "version": {
    <             "requires": "1.0.0"
    <           }
    <         },
    <         {
    <           "group": "com.google.auto",
    <           "module": "auto-common",
    <           "version": {
    <             "requires": "1.2.2"
    <           }
    <         },
    <         {
    125,131d103
    <           "group": "com.google.devtools.ksp",
    <           "module": "symbol-processing-api",
    <           "version": {
    <             "requires": "2.2.10-2.0.2"
    <           }
    <         },
    <         {
    ```

--- compiler/build.gradle.kts
+++ compiler/build.gradle.kts
@@ -4,17 +4,16 @@ plugins {
 }
 
 dependencies {
+    compileOnly(libs.auto.service.api)
+    compileOnly(libs.ksp.api)
+
     implementation(projects.api)
-    implementation(libs.auto.service.api)
-    kapt(libs.auto.service.compiler)
-    implementation(libs.incap.api)
-    kapt(libs.incap.compiler)
-    implementation(libs.auto.common)
     implementation(libs.kotlinpoet.core)
     implementation(libs.kotlinpoet.metadata)
     implementation(libs.kotlinpoet.ksp)
     implementation(libs.kotlin.metadata)
     implementation(libs.oldestSupportedMoshi)
-    implementation(libs.ksp.api)
     implementation(libs.asm)
+
+    kapt(libs.auto.service.compiler)
 }
